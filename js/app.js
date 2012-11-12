060512615706ce89623251dbe438c471230f9430
Joan Roig <roigarderiu@gmail.com>
2012-11-12 22:08:10 +0100

    tried fetching stackoverflow API...

--- js/app.js
+++ js/app.js
@@ -14,13 +14,32 @@ App.AllPlayersView = Ember.View.extend({
 App.ApplicationController = Ember.Controller.extend();
 App.AllPlayersController = Ember.ArrayController.extend();
 
+/* Models? */
+
+App.Player = Ember.Object.extend();
+App.Player.reopenClass({
+    find: function() {
+      $.ajax({
+          url: 'http://api.stackoverflow.com/1.1/tags/ruby/top-answerers/month',
+          dataType: 'jsonp',
+          context: this,
+          success: function(response){
+              response.data.top_users.forEach(function(player){
+                  this.allPlayers.addObject(App.Player.create(player))
+              },this)
+          },
+      });
+      return this.allPlayers;
+    }
+});
+
 
 App.Router = Ember.Router.extend({
   root: Ember.Route.extend({
       index: Ember.Route.extend({
         route: '/',
         connectOutlets: function(router){
-            router.get('applicationController').connectOutlet('allPlayers', [{name:'rivaldo'},{name:'ronaldinho'}]);
+            router.get('applicationController').connectOutlet('allPlayers', App.Player.find());
         }
     })
   })
