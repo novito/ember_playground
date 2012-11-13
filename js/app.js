184567b1eb4b22de327244d601b7fb546300076e
Juan Ramón <juanramon.diaz@gmail.com>
2012-11-13 01:22:49 +0100

    using bower + requirejs to load js files

--- js/app.js
+++ js/app.js
@@ -1,50 +1,60 @@
-App = Ember.Application.create();
-
-/* Views */
-
-App.ApplicationView = Ember.View.extend({
-  templateName: 'application'
-});
-
-App.AllPlayersView = Ember.View.extend({
-  templateName: 'players'
-});
-
-/* Controllers */
-App.ApplicationController = Ember.Controller.extend();
-App.AllPlayersController = Ember.ArrayController.extend();
-
-/* Models? */
-
-App.Player = Ember.Object.extend();
-App.Player.reopenClass({
-    allPlayers: [],
-    find: function() {
-      $.ajax({
-          url: 'https://api.github.com/repos/osclass/OSClass/contributors',
-          dataType: 'jsonp',
-          context: this,
-          success: function(response){
-              console.log(response);
-              response.data.forEach(function(player){
-                  this.allPlayers.addObject(App.Player.create(player))
-              },this)
-          },
-      });
-      return this.allPlayers;
+requirejs.config({
+    paths: {
+        'jquery': '../components/jquery/jquery',
+        'handlebars': '../components/handlebars/handlebars-1.0.0-rc.1',
+        'ember': '../components/ember/ember'
     }
 });
 
+require(['jquery', 'handlebars', 'ember'], function ($) {
+  App = Ember.Application.create();
 
-App.Router = Ember.Router.extend({
-  root: Ember.Route.extend({
-      index: Ember.Route.extend({
-        route: '/',
-        connectOutlets: function(router){
-            router.get('applicationController').connectOutlet('allPlayers', App.Player.find());
-        }
+  /* Views */
+
+  App.ApplicationView = Ember.View.extend({
+    templateName: 'application'
+  });
+
+  App.AllPlayersView = Ember.View.extend({
+    templateName: 'players'
+  });
+
+  /* Controllers */
+  App.ApplicationController = Ember.Controller.extend();
+  App.AllPlayersController = Ember.ArrayController.extend();
+
+  /* Models? */
+
+  App.Player = Ember.Object.extend();
+  App.Player.reopenClass({
+      allPlayers: [],
+      find: function() {
+        $.ajax({
+            url: 'https://api.github.com/repos/osclass/OSClass/contributors',
+            dataType: 'jsonp',
+            context: this,
+            success: function(response){
+                console.log(response);
+                response.data.forEach(function(player){
+                    this.allPlayers.addObject(App.Player.create(player))
+                },this)
+            },
+        });
+        return this.allPlayers;
+      }
+  });
+
+
+  App.Router = Ember.Router.extend({
+    root: Ember.Route.extend({
+        index: Ember.Route.extend({
+          route: '/',
+          connectOutlets: function(router){
+              router.get('applicationController').connectOutlet('allPlayers', App.Player.find());
+          }
+      })
     })
-  })
-});
+  });
 
-App.initialize();
+  App.initialize();
+});
